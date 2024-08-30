f4d153cf24515747dc9af7216a0b93cb01e7c9c0
iamhasibulhasan <hashibul218@gmail.com>
2024-08-30 12:19:46 +0600

    intial setup

---
+++ CleanArchitecture.Domain/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain
+{
+    public sealed class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}

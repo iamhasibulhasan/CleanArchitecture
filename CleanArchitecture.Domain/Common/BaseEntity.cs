c616afd4ed524ec13f562f597caaa801fc945ff1
iamhasibulhasan <hashibul218@gmail.com>
2024-08-30 12:37:06 +0600

    base entity added

---
+++ CleanArchitecture.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
---
+++ CleanArchitecture.Domain/Entities/Leave/LeaveAllocation .cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain.Entities.Leave
+{
+    public sealed class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+        public LeaveType LeaveType { get; set; }
+    }
+}

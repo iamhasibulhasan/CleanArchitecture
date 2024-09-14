faf579cef8ba84c43f72e8a7dda3475ffeec8d89
iamhasibulhasan <hashibul218@gmail.com>
2024-09-14 20:27:57 +0600

    domain entity

--- CleanArchitecture.Domain/Common/BaseEntity.cs
+++ CleanArchitecture.Domain/Common/BaseEntity.cs
@@ -3,9 +3,9 @@
     public abstract class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime DateCreated { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime LastModifiedDate { get; set; }
-        public string LastModifiedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int UpdatedBy { get; set; }
     }
 }

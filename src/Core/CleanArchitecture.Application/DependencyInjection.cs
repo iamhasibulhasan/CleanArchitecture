d6f3dd52a0ad7fc8c6423f3ab1c2348ad1e19984
iamhasibulhasan <hashibul218@gmail.com>
2024-12-24 12:07:46 +0600

    mediatr and fluentvalidation nuget add

--- src/Core/CleanArchitecture.Application/DependencyInjection.cs
+++ src/Core/CleanArchitecture.Application/DependencyInjection.cs
@@ -1,5 +1,21 @@
-﻿namespace CleanArchitecture.Application;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
-public sealed class DependencyInjection
+namespace CleanArchitecture.Application;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuraion =>
+        configuraion.RegisterServicesFromAssembly(assembly));
+
+        services.AddValidatorsFromAssembly(assembly);
+
+
+
+        return services;
+    }
 }
---
+++ src/Core/CleanArchitecture.Infrastructure/Persistence/Configurations/Entities/Users/UserConfig.cs
@@ -0,0 +1,5 @@
+﻿namespace CleanArchitecture.Infrastructure.Persistence.Configurations.Entities.Users;
+
+public sealed class UserConfig
+{
+}

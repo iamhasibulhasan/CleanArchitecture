17a6b2ca1df427e3b40b75ba559426db3c08c28f
iamhasibulhasan <hashibul218@gmail.com>
2024-12-24 12:15:30 +0600

    user config

--- src/Core/CleanArchitecture.Infrastructure/Persistence/Configurations/Entities/Users/UserConfig.cs
+++ src/Core/CleanArchitecture.Infrastructure/Persistence/Configurations/Entities/Users/UserConfig.cs
@@ -1,5 +1,27 @@
-﻿namespace CleanArchitecture.Infrastructure.Persistence.Configurations.Entities.Users;
+﻿using CleanArchitecture.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public sealed class UserConfig
+namespace CleanArchitecture.Infrastructure.Persistence.Configurations.Entities.Users;
+
+public sealed class UserConfig : IEntityTypeConfiguration<User>
 {
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users", "public");
+        builder.HasKey(k => k.Id);
+
+        builder.Property(k => k.UserCode)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder.Property(k => k.FirstName)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder.Property(k => k.LastName)
+            .HasMaxLength(200);
+        builder.Property(k => k.Email)
+            .IsRequired();
+        builder.Property(k => k.Phone)
+            .IsRequired();
+    }
 }

9fe627c7e9b51a5bc2be7a6a233883ca5bf67691
iamhasibulhasan <hashibul218@gmail.com>
2024-12-24 12:20:38 +0600

    generic command and query setup

---
+++ src/Core/CleanArchitecture.Application/Abstraction/MediatR/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Application.Common.Utilities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Abstraction.MediatR;
+
+
+public interface ICommand : IRequest<Result>
+{
+}
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+}
